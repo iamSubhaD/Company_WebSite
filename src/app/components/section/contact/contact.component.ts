b1eee9e198aa382f91e499d1a307053d669b625d
Subha PD <antudey.2014.ad@gmail.com>
2025-05-16 21:55:13 +0530

    finalized services section, finetune done, contact section completed

--- src/app/components/section/contact/contact.component.ts
+++ src/app/components/section/contact/contact.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../../services/language/language.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  faLocationDot = faLocationDot;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faLinkedin = faLinkedin;
+  linkedinUrl = 'https://www.linkedin.com/';
+  faFacebook = faFacebook;
+  facebookUrl = 'https://www.facebook.com/';
+  faInstagram = faInstagram;
+  InstagramUrl = 'https://www.instagram.com/';
+  translation: any = {};
+
+  constructor(private languageService: LanguageService) {
+    this.languageService.translations$.subscribe(data => this.translation = data.contact || {});
+  }
+
 
 }
--- src/app/components/section/services/services.component.ts
+++ src/app/components/section/services/services.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-
+  currentIndex = 0;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
 }
